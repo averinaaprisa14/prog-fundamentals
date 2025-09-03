1f73d29016a77a0d99a5899035eb72fb8042b698
Averina <averinaaprisa14@gmail.com>
2025-09-03 11:28:48 +0700

    file prog-fundamentals.c

--- prog-fundamentals.c
+++ prog-fundamentals.c
@@ -1,5 +1,5 @@
 # include <stdio.h>
 
 int main(){
-	printf("Halo, saya Averina dan bahasa pemrograman yang menarik adalah C")
+	printf("Halo, saya Averina dan bahasa pemrograman yang menarik adalah C");
 }
